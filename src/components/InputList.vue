4a712e54ac5045d5a21b1c480a7d34f9ab477f62
IceMoonMagic <icemoonmagic@gmail.com>
2024-09-13 21:05:41 -0500

    Added Input Repeater
    Allows simple creation of N duplicate inputs without needing to count out and fill N rows.
    
    The array holds each value N times, but adjacent like values get merged in the UI.

--- src/components/InputList.vue
+++ src/components/InputList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Decimal from 'decimal.js'
 import { TrashIcon, PlusIcon } from '@heroicons/vue/16/solid';
+import { computed } from 'vue';
 
 const props = defineProps({
   decimal_places: Number || undefined,
@@ -8,35 +9,82 @@ const props = defineProps({
 })
 const inputs = defineModel<Decimal[]>()
 
+interface SimplifiedRow {
+  index: number;
+  input: Decimal;
+  repeat: number;
+}
+
 function addInput(value: Decimal | number = 0) {
   inputs.value.push(new Decimal(value))
-} 
+}
 
-function updateInput(e: Event, index: number) {
+function updateInput(e: Event, row: SimplifiedRow) {
   if ((e.target as HTMLInputElement).value === "") return
   const e_value = new Decimal((e.target as HTMLInputElement).value)
   const fixed_value = e_value.toDecimalPlaces(props.decimal_places)
 
   // Only fix if values differ, as attempting to add a decimal place did not work
-  if (props.decimal_places === 0 || !inputs.value[index].eq(e_value)) {
-    inputs.value[index] = fixed_value
+  if (props.decimal_places === 0 || !row.input.eq(e_value)) {
+    for (let index = row.index; index < row.index + row.repeat; index++) {
+      inputs.value[index] = fixed_value
+    }
   }
 }
 
-function removeInput(index: number) {
-  inputs.value.splice(index, 1)
+function updateRepeat(e: Event, row: SimplifiedRow) {
+  if ((e.target as HTMLInputElement).value === "") return
+  const e_value = Number((e.target as HTMLInputElement).value)
+  const repeat = Math.floor(e_value)
+  const repeat_change = repeat - row.repeat
+  if (repeat_change > 0) {
+    for (let iter = 0; iter < repeat_change; iter++) {
+      inputs.value.splice(row.index, 0, row.input)
+    }
+  } else if (repeat_change < 0) {
+    for (let iter = 0; iter > repeat_change; iter--) {
+      inputs.value.splice(row.index, 1)
+    }
+  }
+}
+
+function removeInput(row: SimplifiedRow) {
+  inputs.value.splice(row.index, row.repeat)
   if (inputs.value.length === 0) {
     addInput()
   }
 }
+
+const simplifiedInputs = computed<SimplifiedRow[]>(() => {
+  const simplified: SimplifiedRow[] = []
+  for (let index = 0; index < inputs.value.length; index++) {
+    const input = inputs.value[index]
+    if (simplified.length != 0 && simplified[simplified.length - 1].input.eq(input)) {
+      simplified[simplified.length - 1].repeat += 1;
+    }
+    else {
+      simplified.push({
+        index: index,
+        input: input,
+        repeat: 1
+      })
+    }
+  }
+  return simplified
+}
+)
+
 </script>
 
 <template>
   <div class=" space-y-2 outline outline-lavender outline-1 p-2 rounded-lg w-full">
     <label v-if="props.label">{{ props.label }}</label>
-    <div v-for="i in inputs.length" class="flex space-x-2">
-      <input class="rounded-lg p-2 w-full" type="number" min="0" :value="inputs[i-1]" @input="(e) => updateInput(e, i-1)">
-      <button class="latte bg-red text-base" @click="removeInput(i-1)">
+    <div v-for="row in simplifiedInputs" class="flex space-x-2">
+      <input class="rounded-lg p-2 w-full" type="number" min="0" :value="row.input" @input="(e) => updateInput(e, row)">
+      <span> x</span>
+      <input class="rounded-lg p-2 w-full" type="number" min="1" :value="row.repeat"
+        @input="(e) => updateRepeat(e, row)">
+      <button class="latte bg-red text-base" @click="removeInput(row)">
         <TrashIcon class=" size-5 latte text-base" />
       </button>
     </div>
